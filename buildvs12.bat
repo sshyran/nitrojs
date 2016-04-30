508dd88ea51c1e555c976c2bf08a7ad8f2a83039
Simon <nitrologic@gmail.com>
2016-05-01 11:39:55 +1200

    bump

--- buildvs12.bat
+++ buildvs12.bat
@@ -1,7 +1,8 @@
 rem echo off
-mkdir ..\nitrojs_build_vs12
-pushd ..\nitrojs_build_vs12
-cmake -G"Visual Studio 12 2013 Win64" ..\nitrojs
+set nitrojs=%cd%
+mkdir B:\nitrojs_vs12
+pushd B:\nitrojs_vs12
+cmake -G"Visual Studio 12 2013 Win64" %nitrojs%
 REM call "%VS120COMNTOOLS%vsvars32.bat"
 REM MSBuild nitrojs.sln /maxcpucount /p:Configuration=Release
 REM copy Release\nitro.exe ..\..\nitro\bin
